cabeb7a6481a75bdbaebcd2f35676c0d8bbf89ed
David <davidjhoenisch@protonmail.com>
2020-09-26 18:26:17 -0600

    tweaks
    Some minor tweaks for outdate & unneeded syntax.
    
    KNOWN ISSUES: the script stop without any errors part way through the script.  I am working on getting this figured out and fixed

--- scripts/serverdeploy.sh
+++ scripts/serverdeploy.sh
@@ -11,31 +11,30 @@ fi
 apt update -y && apt upgrade -y && apt autoremove -y 
 
 # this section installs the needed admin tools
-sudo apt install chkrootkit lynis qrencode curl git vim
+apt install chkrootkit lynis qrencode curl git vim
 
 # this sections installs the packages needed for WireGuard to work
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+apt update && sudo apt install docker-ce docker-ce-cli containerd.io
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # this section sets some perms and adds needed groups
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-sudo usermod -aG docker $USER
+usermod -aG docker $USER
 
 newgrp docker
 
 # this section will set up a dir
-sudo mkdir /opt/wireguard-server
+mkdir /opt/wireguard-server
 
-sudo chown $USER:$USER /opt/wireguard-server
+chown $USER:$USER /opt/wireguard-server
 
 cat example.txt > /opt/wireguard-server/docker-compose.yaml # this section will need to have some file paths worked out
 
