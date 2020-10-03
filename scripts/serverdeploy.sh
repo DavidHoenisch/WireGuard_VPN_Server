11292dabad4e1335d64b76d6c357cb9f761b967a
David <davidjhoenisch@protonmail.com>
2020-10-02 18:55:54 -0600

    fixes
    fixes to the script

--- scripts/serverdeploy.sh
+++ scripts/serverdeploy.sh
@@ -11,30 +11,30 @@ fi
 apt update -y && apt upgrade -y && apt autoremove -y 
 
 # this section installs the needed admin tools
-apt install chkrootkit lynis qrencode git
+apt install chkrootkit -y lynis -y qrencode -y git -y fail2ban -y
 
 # this sections installs the packages needed for WireGuard to work
-apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt install apt-transport-https -y ca-certificates -y curl -y  gnupg-agent -y software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-apt update && apt install docker-ce docker-ce-cli containerd.io
+apt update -y && apt install docker-ce -y docker-ce-cli -y containerd.io -y
 
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 # this section sets some perms and adds needed groups
 chmod +x /usr/local/bin/docker-compose
 
-usermod -aG docker david
+usermod -aG docker admin
 
 newgrp docker
 
 # this section will set up a dir
 mkdir /opt/wireguard-server
 
-chown david:david /opt/wireguard-server
+chown admin:admin /opt/wireguard-server
 
 cat example.txt > /opt/wireguard-server/docker-compose.yaml # this section will need to have some file paths worked out
 
